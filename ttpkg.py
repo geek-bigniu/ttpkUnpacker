5a63dddffcb59e92d60bbf82abf40d8bf4bdb78c
bigniu <18358411+geek-bigniu@users.noreply.github.com>
2025-06-14 12:40:51 +0800

    新增：支持传入文件夹批量解包功能

--- ttpkg.py
+++ ttpkg.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import os
 import shutil
-
+from os import path
 import ast_parse_ttml_js
 from model.mpk import MPK
 from loguru import logger
@@ -26,14 +26,20 @@ def get_string_by_seperators(source, begin_str, end_str, begin_index):
 
 # 创建目录
 def md(dir):
-    if os.path.exists(dir) is False:
+    if path.exists(dir) is False:
         os.mkdir(dir)
 
 
 # 删除目录
 def rm(dir):
-    if os.path.exists(dir) is True:
+    if path.exists(dir) is True:
         shutil.rmtree(dir)
+
+
+def ex(file):
+    if path.exists(file):
+        return True
+    return False
 
 
 def delete_file(file_path):
@@ -48,12 +54,12 @@ def delete_file(file_path):
     """
     try:
         # 检查文件是否存在
-        if not os.path.exists(file_path):
+        if not path.exists(file_path):
             # logger.info(f"文件 {file_path} 不存在")
             return False
 
         # 确保路径是文件而非目录
-        if not os.path.isfile(file_path):
+        if not path.isfile(file_path):
             # logger.info(f"{file_path} 不是文件")
             return False
 
@@ -80,13 +86,13 @@ def write_to_file(filename, content, output_dir="output"):
         output_dir (str): 输出目录，默认为 "output"
     """
     # 构建完整的文件路径
-    file_path = os.path.join(output_dir, filename)
+    file_path = path.join(output_dir, filename)
 
     # 获取目录路径
-    directory = os.path.dirname(file_path)
+    directory = path.dirname(file_path)
 
     # 如果目录不存在，创建目录
-    if directory and not os.path.exists(directory):
+    if directory and not path.exists(directory):
         os.makedirs(directory)
 
     # 写入文件
@@ -98,6 +104,35 @@ def write_to_file(filename, content, output_dir="output"):
         logger.error(f"写入文件 {file_path} 失败: {e}")
 
 
+def find_pkg_files(directory_path):
+    """
+    查找指定文件夹下所有以 .pkg 或 .ttpkg.js 结尾的文件，并返回文件名列表。
+
+    Args:
+        directory_path (str): 要扫描的文件夹路径。
+
+    Returns:
+        list: 包含符合条件的文件名（不含路径）的列表。
+
+    Raises:
+        ValueError: 如果输入路径无效或不是文件夹。
+    """
+    # 检查输入路径是否有效且是文件夹
+    if not path.exists(directory_path):
+        raise ValueError(f"路径 '{directory_path}' 不存在")
+    if not path.isdir(directory_path):
+        raise ValueError(f"路径 '{directory_path}' 不是文件夹")
+
+    # 获取文件夹中所有以 .pkg 或 .ttpkg.js 结尾的文件名
+    files = [
+        f for f in os.listdir(directory_path)
+        if path.isfile(path.join(directory_path, f))
+           and (f.endswith('.pkg') or f.endswith('.ttpkg.js'))
+    ]
+
+    return files
+
+
 def process_ttss(fname):
     """
     处理 CSS_MAP，解析并将内容写入文件。
@@ -106,18 +141,20 @@ def process_ttss(fname):
         source (str): 包含 CSS_MAP 的源字符串
         output_dir (str): 输出目录，默认为 "output"
     """
-    if os.path.exists(fname) is False:
+    if path.exists(fname) is False:
         return
 
     f = open(fname, "r", encoding='utf-8')
     source = ''.join(f.readlines())
     f.close()
+    if  "window.CSS_MAP" not in source:
+        return
     # 获取 CSS 文件位置
     buf = get_string_by_seperators(source, "Object.assign(window.CSS_MAP||{},", "),window.$m", 0)
     if buf[1] == -1:
         buf = get_string_by_seperators(source, "window.CSS_MAP=", ",window.$m", 0)
     if buf[1] == -1:
-        logger.info("获取 CSS_MAP的TTSS文件 失败，请使用解包后的源文件，格式化后的代码无法解析")
+        logger.warning("获取 CSS_MAP的TTSS文件 失败，请使用解包后的源文件，格式化后的代码无法解析")
         return
     try:
         # 解析 JSON 字符串
@@ -168,7 +205,6 @@ def parseputCssToHead(source, fname):
             index = index if index1 == -1 else index1
             try:
                 buf = eval('[' + j[0:index].replace("undefined", '[]').replace("path", '"path"') + ']')
-                delete_file(fname)
                 filename = fname.replace("-frame.js", '.ttss').replace(OUTPUT_FOLDER + '/', '')
                 content = get_ttss_content(buf[0])
 
@@ -201,14 +237,12 @@ def get_ttss_content(buf):
     return content
 
 
-def process(flist, func):
-    for f in flist:
-        func(OUTPUT_FOLDER + f)
-        with open(OUTPUT_FOLDER + "/app-config.json", "r", encoding='utf-8') as fs:
-            j = json.load(fs)
-            if hasattr(j, "subPackages"):
-                for sub in j["subPackages"]:
-                    func(OUTPUT_FOLDER + "/" + sub["root"] + f)
+def process(fileList, func):
+    for filename in fileList:
+        if not ex(filename):
+            logger.warning(f"文件不存在：{filename}，可能是子包文件")
+            continue
+        func(filename)
 
 
 def processPageTtss():
@@ -216,13 +250,16 @@ def processPageTtss():
     解析子目录的ttss文件
     :return:
     """
-    with open(OUTPUT_FOLDER + "/app-config.json", "r", encoding='utf-8') as fs:
+    file_name = OUTPUT_FOLDER + "/app-config.json"
+    if not ex(file_name):
+        return
+    with open(file_name, "r", encoding='utf-8') as fs:
         j = json.load(fs)
         if "pages" in j:
             for page in j["pages"]:
 
                 file_path = OUTPUT_FOLDER + "/" + page + "-frame.js"
-                if os.path.exists(file_path) is False:
+                if path.exists(file_path) is False:
                     continue
                 f = open(file_path, "r", encoding='utf-8')
                 source = ''.join(f.readlines())
@@ -235,8 +272,10 @@ def processPageJSON():
     解析子目录的ttss文件
     :return:
     """
-    jsonFile = "/app-config.json"
-    fs = open(OUTPUT_FOLDER + jsonFile, "r", encoding='utf-8')
+    jsonFile = OUTPUT_FOLDER + "/app-config.json"
+    if not ex(jsonFile):
+        return
+    fs = open(jsonFile, "r", encoding='utf-8')
     j = json.load(fs)
     fs.close()
     if "page" in j:
@@ -270,29 +309,34 @@ def processPageJSON():
 
 def process_package(file):
     global OUTPUT_FOLDER
+    fileList = []
     with open(file, 'rb') as pkgfile:
         mpk = MPK.load(pkgfile)
         for i in mpk.files:
             file = mpk.file(i)
+            fileList.append(file['name'])
             if file['offset'] != 0:
                 if file['name'] == '':
                     file['name'] = 'unknown_%s' % i
                 logger.info('Unpacking: %s' % file['name'])
-                path_file = '%s/%s' % (OUTPUT_FOLDER, file['name'])
-                dir_file, _ = os.path.split(path_file)
+                path_file = path.join(OUTPUT_FOLDER, file['name'])
+                dir_file, _ = path.split(path_file)
                 os.makedirs(dir_file, exist_ok=True)
-                with open(path_file, 'wb') as io_file:
+                with open(path_file, 'wb+') as io_file:
                     io_file.write(mpk.data(i))
+                    io_file.flush()
+                    io_file.close()
+    return fileList
 
 
-# OUT_PATH = "js/038d897.ttpkg.js_unpack/"
-
-def processTtmlByAst(file_path):
+def processTtmlByAst(input_file_name):
     """
-
-    :return:
+    ast解析js转成ttml文件
     """
-    f = open(OUTPUT_FOLDER + file_path, "r", encoding='utf-8')
+    if not ex(input_file_name):
+        logger.warning(f"文件不存在：{input_file_name}，可能是子包文件")
+        return
+    f = open(input_file_name, "r", encoding='utf-8')
     source = ''.join(f.readlines())
     f.close()
     pageJs = source.split("window.$m_")[1:]
@@ -309,7 +353,7 @@ def processTtmlByAst(file_path):
             pageList[pageTag] = filename
             logger.info(f"Decompiling {filename}.ttml")
             astJs = page[0:file_name_begin - len(begin_str) - 1]
-            ast_parse_ttml_js.run(astJs, pageList, OUTPUT_FOLDER + "/" + filename + ".ttml")
+            ast_parse_ttml_js.run(astJs, pageList, path.join(OUTPUT_FOLDER, filename + ".ttml"))
 
 
 def end():
@@ -322,46 +366,46 @@ def end():
         delete_file(OUTPUT_FOLDER + "/" + file_name)
 
 
-def main(output_folder, input_file):
+def main(input_file):
     """
     主函数，执行解包和反编译流程
     参数:
-        output_folder: 输出文件夹路径
         input_file: 输入的小程序包文件路径
+        output_folder: 输出文件夹路径
         file: 要处理的全局页面文件路径
     """
     global OUTPUT_FOLDER
-    file = '/page-frame.js'
-    OUTPUT_FOLDER = output_folder  # 设置全局变量
-    # 删除输出文件夹
-    rm(OUTPUT_FOLDER)
     # 创建输出文件夹
     md(OUTPUT_FOLDER)
     # 开始解包小程序包
-    logger.info("Unpacking package...")
-    process_package(input_file)
+    logger.info(f"Unpacking {input_file} package...")
+    fileList = process_package(input_file)
+    pageFrameList = []
+    for fileName in fileList:
+        name = path.join(OUTPUT_FOLDER, fileName)
+        if path.isfile(name) and fileName.endswith("-frame.js"):
+            pageFrameList.append(name)
     # 反编译全局的 ttss 文件
-    process([file], process_ttss)
+    process(pageFrameList, process_ttss)
     # 反编译每个界面的 ttss 文件
     processPageTtss()
     # 构造 page.json 文件
     processPageJSON()
     # 通过 AST 反编译 ttml 文件
-    processTtmlByAst(file)
+    process(pageFrameList, processTtmlByAst)
     logger.info("Operation completed successfully!")
 
 
 if __name__ == "__main__":
     # 创建命令行参数解析器
-    parser = argparse.ArgumentParser(description="pkg文件路径")
+    parser = argparse.ArgumentParser(description="抖音解包工具v1.0 beta")
     parser.add_argument(
         "-i",
-        "--input_file",
+        "--input",
         type=str,
         required=True,
-        help="输入pkg文件 (e.g., 'js/e2670a8.pkg')"
+        help="输入pkg文件或文件夹 (e.g., 'js/e2670a8.pkg' or 'js/')"
     )
-    # 添加简写参数
     parser.add_argument(
         "-o",
         "--output_folder",
@@ -369,8 +413,20 @@ if __name__ == "__main__":
         default="output",
         help="输出目录 (默认: 'output')"
     )
-
     # 解析命令行参数
     args = parser.parse_args()
-    # 调用主函数，传入解析的参数
-    main(args.output_folder, args.input_file)
+    OUTPUT_FOLDER = args.output_folder
+    input_arg = args.input
+    if not ex(input_arg):
+        logger.error(f"输入文件或文件夹不存在，请确认文件路径，当前输入文件：{input_arg}")
+    if path.isfile(input_arg):
+        # 单文件处理
+        main(input_arg)
+    else:
+        # 文件夹下所有pkg文件处理
+        pkg_files = find_pkg_files(input_arg)
+        if len(pkg_files) == 0:
+            logger.error(f"{input_arg}目录下未找到pkg文件，请确认路径是否正确")
+        for pkg_file in pkg_files:
+            main(path.join(input_arg, pkg_file))
+        pass
